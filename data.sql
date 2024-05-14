fb468b13449e60efa485c4cc04281d6cdb23fa3b
Emiiliinjs <168409871+Emiiliinjs@users.noreply.github.com>
2024-05-14 13:06:29 +0300

    table users(login)

--- data.sql
+++ data.sql
@@ -16,3 +16,10 @@ INSERT INTO tasks
 VALUES
 ("zvetēt sievu","ar mietu", "2023-05-14", "2023-05-15",0),
 ("slānīt bērnus", "ar siksnu", "2023-05-14", "2023-05-16",0);
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    admin BOOLEAN NOT NULL DEFAULT false
+);
