5f3681fd919175f12d64f584c9b7fe0256ef979b
SkolnieksAdmin <skolnieksadmin@admins-iMac-8.local>
2024-05-15 11:03:04 +0300

    elvis

--- App/Views/index.view.php
+++ App/Views/index.view.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login and Register</title>
+    <link rel="stylesheet" href="../Public/CSS/index.css">
 </head>
 <body>
-    asdads
+    <div class="container">
+        <div class="header">
+            <img src="../Public/IMAGES/logo.png" alt="Logo" class="logo">
+            <h1>To Do List</h1>
+        </div>
+        <div class="buttons">
+            <a href="../user/login" class="btn">Login</a>
+            <a href="../user/register" class="btn">Register</a>
+        </div>
+    </div>
 </body>
 </html>
