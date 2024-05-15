0095176df5531ac282bd9222f1f644e09ac31b3a
SkolnieksAdmin <skolnieksadmin@admins-iMac-4.local>
2024-05-15 14:00:58 +0300

    navbar added

--- App/Views/dashboard-view.php
+++ App/Views/dashboard-view.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=\, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    hello
-</body>
-</html>
+<?php require "../App/Views/components/user.navbar.php" ?>
