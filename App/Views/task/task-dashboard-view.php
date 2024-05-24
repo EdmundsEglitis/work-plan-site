1c2225f7bc0b9afc6d2f3487d6764c65a88b5c79
greralfs <165504543+greralfs@users.noreply.github.com>
2024-05-24 09:24:14 +0300

    Update task-dashboard-view.php

--- App/Views/task/task-dashboard-view.php
+++ App/Views/task/task-dashboard-view.php
@@ -1,18 +1,36 @@
+<?php require "../App/Views/components/user.navbar.php" ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width= , initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../Public/CSS/dashboard.css">
+    
 </head>
 <body>
-
+<br>
+<br>
+<br>
+<br>
+<div class="title-div">
+    <h1 id="tasks-title">Tasks:</h1>
+</div>
+<br>
 <?php
+if (!empty($tasks)) {
     foreach ($tasks as $task) {
-        echo '<div>';
-        echo '<h1>' . $task['Title'] . '</h1>';
-        echo '<p>' . $task['Status'] . '</p>';
+        echo '<div class="card">';
+        echo '<h2>' . $task['Title'] . '</h2>';
+        if ($task['Status'] == 'done') {
+            echo '<p class="done">' . $task['Status'] . '</p>';
+        } else {
+            echo '<p class="not-done">' . $task['Status'] . '</p>';
+        }
         echo '</div>';
-    }?>
+    }
+}
+?>
+
 </body>
 </html>
