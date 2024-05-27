14f2154be42093b0e5f184813c66f1d5dc2e825a
Emiiliinjs <168409871+Emiiliinjs@users.noreply.github.com>
2024-05-27 09:16:58 +0300

    Update user.navbar.php

--- App/Views/components/user.navbar.php
+++ App/Views/components/user.navbar.php
@@ -3,5 +3,6 @@
     
         <a href="/task/dashboard" class="nav-item">Dashboard</a>
         <a href="/task/index" class="nav-item">Create</a>
+        <a href="/task/calendar" class="nav-item">Calendar</a>
         <a href="/user/logout" class="nav-item">Logout</a>
     </nav>
