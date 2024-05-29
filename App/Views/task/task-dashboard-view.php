703ec1bd7fc53025eaf6baf2cdeebfe4bb9e7e13
greralfs <greralfs@gmail.com>
2024-05-29 13:55:32 +0300

    show
    show css

--- App/Views/task/task-dashboard-view.php
+++ App/Views/task/task-dashboard-view.php
@@ -1,43 +1,37 @@
-
-<?php require "../App/Views/components/user.navbar.php" ?><!DOCTYPE html>
+<?php require "../App/Views/components/user.navbar.php" ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width= , initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="../Public/CSS/dashboard.css">
 </head>
 <body>
-<br>
-<br>
-<br>
-<br>
-<div class="title-div">
-    <h1 id="tasks-title">Tasks:</h1>
-</div>
-<br>
-<?php
-if (!empty($tasks)) {
-    foreach ($tasks as $task) {
-        echo '<div class="card">';
-        echo '<h2>' . $task['Title'] . '</h2>';
-        if ($task['Status'] == 'done') {
-            echo '<p class="done">' . $task['Status'] . '</p>';
-        } else {
-            echo '<p class="not-done">' . $task['Status'] . '</p>';
+    <div class="title-div">
+        <h1 id="tasks-title">Tasks:</h1>
+    </div>
+    <div class="wrapper">
+        <?php
+        if (!empty($tasks)) {
+            foreach ($tasks as $task) {
+                echo '<div class="card">';
+                echo '<h2>' . $task['Title'] . '</h2>';
+                if ($task['Status'] == 'done') {
+                    echo '<p class="done">' . $task['Status'] . '</p>';
+                } else {
+                    echo '<p class="not-done">' . $task['Status'] . '</p>';
+                }
+                ?>
+                <form method="post" action="/task/show" class="show-form">
+                    <input type="hidden" name="id" value="<?= $task['id'] ?>">
+                    <button type="submit" class="show-view-button">View Task</button>
+                </form>
+                <?php
+                echo '</div>';
+            }
         }
         ?>
-
-        <form method="post" action="/task/show" class="show-form">
-        <input type="hidden" name="id" value="<?= $task['id'] ?>">
-        <button type="submit" class="show-view-button">View Task</button>
-        </form>
-        <?php
-        echo '</div>';
-        
-    }
-}
-?>
-
+    </div>
 </body>
 </html>
--- App/Views/task/task-show-view.php
+++ App/Views/task/task-show-view.php
@@ -1,15 +1,16 @@
+<?php require "../App/Views/components/user.navbar.php" ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="stylesheet" href="../Public/CSS/dashboard.css">
+    <link rel="stylesheet" href="../Public/CSS/show.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Task Details</title>
 </head>
 <body>
 <div class="card">
-                    <h2><?= $task['Title'] ?></h2>
-                    <p class="<?= ($task['Status'] == 'done') ? 'done' : 'not-done' ?>"><?= $task['Status'] ?></p>
+    <h2><?= $task['Title'] ?></h2>
+    <p class="<?= ($task['Status'] == 'done') ? 'done' : 'not-done' ?>"><?= $task['Status'] ?></p>
 </div>
 </body>
 </html>
