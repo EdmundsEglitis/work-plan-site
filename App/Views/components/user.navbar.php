cb340d4d64d145d8306f41fdfbda5602a0a207f7
greralfs <greralfs@gmail.com>
2024-05-31 10:39:15 +0300

    navbar, calendar, dashboard update
    Weeee

--- App/Views/components/user.navbar.php
+++ App/Views/components/user.navbar.php
@@ -1,8 +1,25 @@
-<link rel="stylesheet" href="../Public/CSS/navbar.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+    <link rel="stylesheet" href="../Public/CSS/navbar.css">
+</head>
+<body>
     <nav class="navbar">
-    
-        <a href="/task/dashboard" class="nav-item">Dashboard</a>
-        <a href="/task/calendar" class="nav-item">Calendar</a>
-        <a href="/task/index" class="nav-item">Create</a>
-        <a href="/user/logout" class="nav-item">Logout</a>
+        <div class="navbar-brand">
+            <button class="navbar-toggler" id="navbar-toggler">
+                &#9776; <!-- Hamburger icon -->
+            </button>
+        </div>
+        <ul class="navbar-menu" id="navbar-menu">
+            <li class="navbar-item"><a href="/task/dashboard">Dashboard</a></li>
+            <li class="navbar-item"><a href="/task/calendar">Calendar</a></li>
+            <li class="navbar-item"><a href="/task/index">Create</a></li>
+            <li class="navbar-item"><a href="/user/logout">Logout</a></li>
+        </ul>
     </nav>
+    <script src="../Public/JS/navbar.js"></script>
+</body>
+</html>
