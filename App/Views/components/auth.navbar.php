2c3b4afd729fe331ab6f82dbc9ad6f398de1cdbd
greralfs <greralfs@gmail.com>
2024-06-03 12:42:01 +0300

    navbar update burger
    mmm yummy burgur

--- App/Views/components/auth.navbar.php
+++ App/Views/components/auth.navbar.php
@@ -1,5 +1,23 @@
-<link rel="stylesheet" href="../Public/CSS/navbar.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="../Public/CSS/navbar.css">
+</head>
+<body>
     <nav class="navbar">
-        <a href="/user/register" class="nav-item">Register</a>
-        <a href="/user/login" class="nav-item">Login</a>
+        <div class="navbar-brand">
+            <button class="navbar-toggler" id="navbar-toggler">
+                &#9776; <!-- Hamburger icon -->
+            </button>
+        </div>
+        <ul class="navbar-menu" id="navbar-menu">
+            <li class="navbar-item"><a href="/user/register">Register</a></li>
+            <li class="navbar-item"><a href="/user/login">Login</a></li>
+        </ul>
     </nav>
+    <script src="../Public/JS/navbar.js"></script>
+</body>
+</html>
